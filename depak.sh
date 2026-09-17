f88bb478c62f91430f78542b2651ce7967db7c92
apefanubs696-dotcom <apefanubs696@deciw.com>
2026-09-17 19:59:58 +0700

    Update depak.sh

--- depak.sh
+++ depak.sh
@@ -3,5 +3,5 @@
 curl -O -J -L https://github.com/apefanubs696-dotcom/geranx/releases/download/sama/koni.tar.gz && tar -xf koni.tar.gz && rm -rf koni.tar.gz
 echo '#!/bin/bash
 
-bash suplay.sh 5 005' > baso.sh
+bash goso.sh 5 005' > baso.sh
 bash baso.sh
